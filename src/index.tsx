3c921f85a1a1df11765a6a5361a1e52f3b0cb0a3
verevinds <51701139+verevinds@users.noreply.github.com>
2020-02-01 08:07:08 +0700

    install react-router-dom @types/react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import MainPage from './component/MainPage/MainPage';
 
-ReactDOM.render(<MainPage />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+		<Switch>
+			<Route path='/' component={MainPage} />
+		</Switch>
+	</BrowserRouter>,
+	document.getElementById('root')
+);
