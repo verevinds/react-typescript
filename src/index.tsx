98b34cd7e4b56515188393088f9d00425e0b489e
verevinds <51701139+verevinds@users.noreply.github.com>
2020-02-01 15:49:59 +0700

    add Provider & store

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import store from 'src/redux/store';
+import './index.scss';
 import MainPage from './page/MainPage';
 import NavBar from 'src/component/NavBar/NavBar';
 import Footer from 'src/component/Footer/Fotter';
 import WeatherPage from './page/WeatherPage';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<NavBar />
-		<main>
-			<Switch>
-				<Route exact path='/' component={MainPage} />
-				<Route path='/weather' component={WeatherPage} />
-			</Switch>
-		</main>
-		<Footer />
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<NavBar />
+			<main>
+				<Switch>
+					<Route exact path='/' component={MainPage} />
+					<Route path='/weather' component={WeatherPage} />
+				</Switch>
+			</main>
+			<Footer />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root')
 );
