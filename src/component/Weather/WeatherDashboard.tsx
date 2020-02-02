cf5fd39d6689ebfedd4ca33583725faf99e95367
verevinds <51701139+verevinds@users.noreply.github.com>
2020-02-02 18:44:11 +0700

    refactoring code - made readable

--- src/component/Weather/WeatherDashboard.tsx
+++ src/component/Weather/WeatherDashboard.tsx
@@ -3,48 +3,57 @@ import './WeatherDashboard.scss';
 import 'src/scss/accent.scss';
 import { IWeatherDashboars } from 'src/interface';
 
-const WeatherDashboars = (props: IWeatherDashboars) => {
-	let { location, current } = props;
+const WeatherDashboars = ({ location, current }: IWeatherDashboars) => {
+	const { localtime, name } = location;
+	const {
+		weather_icons,
+		temperature,
+		wind_speed,
+		wind_dir,
+		pressure,
+		humidity,
+		cloudcover,
+		uv_index,
+		weather_descriptions
+	} = current;
+
 	return (
 		<div className={'dashboard'}>
 			<div className={'dashboard__title'}>
-				<h2>{location.name}</h2>
-				<img
-					src='https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png'
-					alt='weather-icon'
-				/>
+				<h2>{name}</h2>
+				<img src={weather_icons[0]} alt='weather-icon' />
 			</div>
 
 			<div className='dashboard__info-panel'>
 				<div>
-					<p>Температура:</p> <p className='accent-a'>{current.temperature}&deg;</p>
+					<p>Температура:</p> <p className='accent-a'>{temperature}&deg;</p>
 				</div>
 				<div>
-					<p>Сила ветра:</p> <p className='accent-f'>{current.wind_speed}км/ч</p>
+					<p>Сила ветра:</p> <p className='accent-f'>{wind_speed}км/ч</p>
 				</div>
 				<div>
-					<p>Направление ветра:</p> <p className='accent-c'>{current.wind_dir}</p>
+					<p>Направление ветра:</p> <p className='accent-c'>{wind_dir}</p>
 				</div>
 				<div>
-					<p>Давление:</p> <p className='accent-e'>{current.pressure} MB</p>
+					<p>Давление:</p> <p className='accent-e'>{pressure} MB</p>
 				</div>
 				<div>
-					<p>Влажность:</p> <p className='accent-b'>{current.humidity}%</p>
+					<p>Влажность:</p> <p className='accent-b'>{humidity}%</p>
 				</div>
 				<div>
 					<p>Облачность:</p>
-					<p>{current.cloudcover}</p>
+					<p>{cloudcover}</p>
 				</div>
 				<div>
 					<p>УФ-индекс:</p>
-					<p>{current.uv_index}</p>
+					<p>{uv_index}</p>
 				</div>
 				<div>
-					<p>{current.weather_descriptions}</p>
+					<p>{weather_descriptions}</p>
 				</div>
 			</div>
 			<div className='dashboard__status'>
-				<p>{location.localtime}</p>
+				<p>{localtime}</p>
 			</div>
 		</div>
 	);
