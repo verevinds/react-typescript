c9937564aeaa6884b72feb0b16273af0dea41dc2
verevinds <51701139+verevinds@users.noreply.github.com>
2020-02-02 18:42:52 +0700

    refactoring interface

--- src/interface.ts
+++ src/interface.ts
@@ -55,6 +55,33 @@ export interface IImgLoaderItem {
 }
 /**Weather */
 export interface IWeatherDashboars {
-	location: any;
-	current: any;
+	location: {
+		name: string;
+		country: string;
+		region: string;
+		lat: string;
+		lon: string;
+		timezone_id: string;
+		localtime: string;
+		localtime_epoch: number;
+		utc_offset: string;
+	};
+	current: {
+		observation_time: string;
+		temperature: number;
+		weather_code: number;
+		weather_icons: string[];
+		weather_descriptions: string[];
+		wind_speed: number;
+		wind_degree: number;
+		wind_dir: string;
+		pressure: number;
+		precip: number;
+		humidity: number;
+		cloudcover: number;
+		feelslike: number;
+		uv_index: number;
+		visibility: number;
+		is_day: string;
+	};
 }
